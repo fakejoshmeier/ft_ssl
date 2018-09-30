e522b362e14e3c550541d5ac24093cfb84cb6110
Joshua Meier <joshua.meier2@gmail.com>
2018-09-29 17:14:19 -0700

    Fixed input

--- src/input.c
+++ src/input.c
@@ -6,7 +6,7 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/17 14:53:38 by jmeier            #+#    #+#             */
-/*   Updated: 2018/07/28 00:54:39 by jmeier           ###   ########.fr       */
+/*   Updated: 2018/09/28 23:05:12 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,27 @@
 
 char	*stdin_in(t_ssl *ssl)
 {
+	char	*tmp;
 	char	*ret;
 	char	buf[256];
 	int		i;
 
 	ret = ft_strnew(0);
 	ssl->in_size = 0;
-	while ((i = read(0, &buf, 255)))
+	while ((i = read(0, buf, 255)))
 	{
 		if (i == -1)
 		{
 			ft_printf("ft_ssl: read error: failed to read from stdin\n");
-			ft_free(ret);
-			return (NULL);
-		}
-		buf[i] = '\0';
-		if (!(ret = ft_strfjoin(ret, buf)))
-		{
-			ft_printf("ft_ssl: Malloc fail\n");
 			free(ret);
 			return (NULL);
 		}
+		NULL_GUARD(tmp = ft_strnew(i + ssl->in_size));
+		ft_memcpy(tmp, ret, ssl->in_size);
+		ft_memcpy(&tmp[ssl->in_size], buf, i);
 		ssl->in_size += i;
+		free(ret);
+		ret = tmp;
 	}
 	return (ret);
 }
